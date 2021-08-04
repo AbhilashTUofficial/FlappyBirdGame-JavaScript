d3349cbaf11f2fa01cca6d12cd59df2f571b1291
abhilashTUofficial <abhilashstorm@gmail.com>
2021-08-05 03:04:02 +0530

    update

---
+++ README.md
@@ -0,0 +1,44 @@
+# Flappy Bird Game - JavaScript
+This is a fully functional responsive Flappy-bird game in JavaScript.
+<br/>
+### Languages & tools :
+[<img align="left" alt="VS Code" width="28px" src="./Img/vscode.png">][vs]
+[<img align="left" alt="JavaScript" width="26px" src="./Img/javascript.svg">][js]
+<br/><br/>
+![GitHub last commit](https://img.shields.io/github/last-commit/AbhilashTUofficial/FlappyBirdGame-JavaScript?color=blue&label=Last%20Commit%3A&style=for-the-badge)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/AbhilashTUofficial/FlappyBirdGame-JavaScript?label=Repo%20Size%3A&style=for-the-badge)
+|FlappyBirdGame-JavaScript|
+|------|
+|<img src="./Img/screenshot.png" width="400">|
+
+
+### Directory Structure :
+    FlappyBirdGame-JavaScript
+    |
+    |-- index.html
+    |-- style.css
+    |-- script.js
+    |
+    |-- Img
+    |    |-- vscode.png
+    |    |-- javascript.svg
+    |    |-- screenshot.png
+    |    |-- bird.png
+    |
+    |-- README.md
+## Connect with me:  
+<a href="https://grabify.link/34LU2G" target="_blank">
+<img src=https://img.shields.io/badge/twitter-%2300acee.svg?&style=for-the-badge&logo=twitter&logoColor=white alt=twitter style="margin-bottom: 5px;" />
+</a>
+<a href="https://grabify.link/A9HVHU" target="_blank">
+<img src=https://img.shields.io/badge/facebook-%232E87FB.svg?&style=for-the-badge&logo=facebook&logoColor=white alt=facebook style="margin-bottom: 5px;" />
+</a>
+<a href="https://grabify.link/T0ZFYZ/" target="_blank">
+<img src=https://img.shields.io/badge/instagram-%23000000.svg?&style=for-the-badge&logo=instagram&logoColor=white alt=instagram style="margin-bottom: 5px;" />
+</a>  
+<br/>
+
+[website]: https://abhilashtuofficial.github.io/
+[js]: https://github.com/AbhilashTUofficial/JavaScript-programming
+[vs]: https://github.com/AbhilashTUofficial/AbhilashTUofficial
+
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var block_A=document.getElementById('block_A');
-var hole_A=document.getElementById('hole_A');
-var blockBottom_A=document.getElementById('block-bottom_A');
-var blockTop_A=document.getElementById('block-top_A');
+var block=document.getElementById('block');
+var hole=document.getElementById('hole');
+var blockBottom=document.getElementById('block-bottom');
+var blockTop=document.getElementById('block-top');
 var gameOverLabel=document.getElementById('game-over-label');
 var bird=document.getElementById('bird');
 var jumping=false;
 var gameOver=false;
 
 
-hole_A.addEventListener('animationiteration',()=>{
+hole.addEventListener('animationiteration',()=>{
     if(!gameOver){
-        var random_A=-((Math.random()*400)+150);
-        hole_A.style.top=random_A+'px';
-        blockBottom_A.style.top=random_A+'px';
-        blockTop_A.style.top=random_A-190+'px';
+        var random=-((Math.random()*400)+150);
+        hole.style.top=random+'px';
+        blockBottom.style.top=random+'px';
+        blockTop.style.top=random-190+'px';
     }else{
-        block_A.style.animation="none"
-        blockTop_A.style.animation="none"
-        blockBottom_A.style.animation="none"
+        block.style.animation="none"
+        blockTop.style.animation="none"
+        blockBottom.style.animation="none"
         gameOverLabel.textContent="Game Over!"
     }
 
@@ -31,6 +31,24 @@ setInterval(function(){
     }
     if(birdTop==830){
         gameOver=true;
+    }
+    high=hole.offsetTop
+    low=hole.offsetTop+150
+    if(hole.offsetLeft<540){
+        gameOver=true
+        if(bird.offsetTop>high){
+            if(bird.offsetTop<low){
+                gameOver=false
+                console.log("entered")
+            }
+        }
+        if(gameOver){
+            document.addEventListener("click",handler,true);
+            function handler(e){
+                e.stopPropagation();
+                e.preventDefault();
+            }
+        }
     }
 },10);
 
@@ -52,3 +70,47 @@ function jump(){
         jumpCount++; 
     },10 );
 } 
+
+
+// var block = document.getElementById("block");
+// var hole = document.getElementById("hole");
+// var character = document.getElementById("character");
+// var jumping = 0;
+// var counter = 0;
+
+// hole.addEventListener('animationiteration', () => {
+//     var random = -((Math.random()*300)+150);
+//     hole.style.top = random + "px";
+//     counter++;
+// });
+// setInterval(function(){
+//     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//     if(jumping==0){
+//         character.style.top = (characterTop+3)+"px";
+//     }
+//     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+//     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+//     var cTop = -(500-characterTop);
+//     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+//         alert("Game over. Score: "+(counter-1));
+//         character.style.top = 100 + "px";
+//         counter=0;
+//     }
+// },10);
+
+// function jump(){
+//     jumping = 1;
+//     let jumpCount = 0;
+//     var jumpInterval = setInterval(function(){
+//         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//         if((characterTop>6)&&(jumpCount<15)){
+//             character.style.top = (characterTop-5)+"px";
+//         }
+//         if(jumpCount>20){
+//             clearInterval(jumpInterval);
+//             jumping=0;
+//             jumpCount=0;
+//         }
+//         jumpCount++;
+//     },10);
+// }
